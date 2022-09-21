64c1354382426c1c323e862042611cf72548148c
whrod <whrodtube@gmail.com>
2022-09-21 19:50:20 +0900

    migrations 수정 stylecode - unique discountprice - null, default, desciption - null, default

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Listening to request on 127.0.0.1:${PORT}`);
 });
+
+/*
+stylecode - unique
+discountprice - null, default,
+desciption - null, default,
+*/
