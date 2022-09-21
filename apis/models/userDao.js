9f9d75ca21b9873fe18883ef525a073da44db756
whrod <whrodtube@gmail.com>
2022-09-21 20:18:34 +0900

    signin

--- apis/models/userDao.js
+++ apis/models/userDao.js
@@ -6,13 +6,25 @@ const database = require('./dataSource');
 //     )
 // }
 
-// const signIn = async () => {
-//     await database.query(
-
-//     )
-// }
+const getUserByUsername = async (username) => {
+    await database.query(
+        `SELECT
+            id,
+            username,
+            fullname,
+            password,
+            phone_number,
+            address,
+            gender,
+            birth
+        FROM users
+        WHERE = ?
+        `,
+        [username]
+    )
+}
 
 module.exports = {
     // signUp,
-    // signIn,
+    getUserByUsername
 }
--- apis/services/userService.js
+++ apis/services/userService.js
@@ -1,14 +1,36 @@
 const { userDao } = require('../models');
+const bcyrpt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { validateUsername, validatePassword } = require('../utils/validators')
 
 // const signUp = async () => {
 
 // }
 
-// const signIn = async () => {
+const signIn = async (username, password) => {
 
-// }
+    validateUsername(username);
+    validatePassword(password);
+
+    const user = await.userDao.getUserByUsername();
+    const passwordMatch = await bcyrpt.compare(password, user.password)
+
+    if(!passwordMatch) {
+        const error = new Error('WRONG_PASSWORD')
+        error.statusCode = 400
+
+        throw error;
+    }
+
+    return accessToken = jwt.sign({id:user.id}, process.env.JWT_SECRET,
+        {
+            algorithm : process.env.ALGORITHM,
+            expiresIn : process.env.JWT_EXPIRES_IN
+        }
+    )
+};
 
 module.exports = {
     // signUp,
-    // signIn,
+    signIn,
 }
