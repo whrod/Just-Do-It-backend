5afbe0a2ecd39c8f4a1ae94673b3150343cf6e41
whrod <whrodtube@gmail.com>
2022-09-22 01:33:09 +0900

    마이그레이션 수정 주석삭제

--- app.js
+++ app.js
@@ -18,9 +18,3 @@ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`Listening to request on 127.0.0.1:${PORT}`);
 });
-
-/*
-stylecode - unique
-discountprice - null, default,
-desciption - null, default,
-*/
