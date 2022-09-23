e3a893ea00f566d7f0b73e70995ee88f9a4fff65
whrod <whrodtube@gmail.com>
2022-09-23 14:22:34 +0900

    [ADD] getCart 기능 구현 중간저장

--- apis/models/cartDao.js
+++ apis/models/cartDao.js
@@ -1,17 +1,19 @@
 const database = require('../models/dataSource');
+const { cartService } = require('../services');
 
 const getCartById = async (userId) => {
   const cart = await database.query(
     `
     SELECT DISTINCT
-            p.name AS productName,
-            c.quantity,
-            s.foot_size AS size,
-            p.retail_price AS retailPrice,
-            o.stock,
-            p.style_code AS styleCode,
-            p.discount_price AS discountPrice,
-            p.thumbnail
+        c.id AS cartId,
+        p.name AS productName,
+        c.quantity,
+        s.foot_size AS size,
+        p.retail_price AS retailPrice,
+        o.stock,
+        p.style_code AS styleCode,
+        p.discount_price AS discountPrice,
+        p.thumbnail
     FROM carts c, product_options o, products p, sizes s
     WHERE c.product_option_id = o.id
     AND o.product_id = p.id 
