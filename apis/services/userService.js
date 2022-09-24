63130f4894df0975e7b465ab97a91fde5327b8bb
whrod <whrodtube@gmail.com>
2022-09-24 15:03:22 +0900

    signin response fullName, select 변수명 수정

--- apis/services/userService.js
+++ apis/services/userService.js
@@ -2,8 +2,8 @@ const { userDao } = require('../models');
 const bcyrpt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const signIn = async (username, password) => {
-  const user = await userDao.getUserByUsername(username);
+const signIn = async (userName, password) => {
+  const user = await userDao.getUserByUsername(userName);
 
   if (user === undefined) {
     const error = new Error('INVALID_USER');
@@ -21,10 +21,12 @@ const signIn = async (username, password) => {
     throw error;
   }
 
-  return (accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+  const accessToken = await jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
     algorithm: process.env.ALGORITHM,
     expiresIn: process.env.JWT_EXPIRES_IN,
-  }));
+  });
+
+  return await [user.fullName, accessToken];
 };
 
 const getUserById = async (id) => {
