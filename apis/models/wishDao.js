cf2204c6acf9b4b3a72d1118ba4f0d2d4146c84a
yunah <a1@1ui-MacBookPro-2.local>
2022-09-27 23:23:04 +0900

    wip

---
+++ apis/models/wishDao.js
@@ -0,0 +1,76 @@
+const database = require('./dataSource');
+
+const createWish = async (productId, userId) => {
+  try {
+    return await database.query(
+      `INSERT INTO wishlist(
+        product_id,
+        user_id
+        ) VALUES(?, ?)
+    `, [productId, userId]
+    );
+  } catch (err) {
+    const error = new Error('INVALID_DATA_INPUT');
+    error.statusCode = 500;
+    throw error;
+  }
+}
+
+const showWish = async (userId) => {
+  try {
+    return await database.query(
+      `SELECT
+      p.thumbnail AS thumbnail,
+      p.name AS name,
+      IFNULL(p.discount_price, p.retail_price) AS price,
+      u.id AS userId
+      FROM products p
+      JOIN wishlist w ON w.product_id = p.id
+      JOIN users u ON u.id = w.user_id
+      WHERE w.user_id = ?
+    `, [userId]
+    );
+  } catch (err) {
+    const error = new Error('INVALID_DATA_INPUT');
+    error.statusCode = 500;
+    throw error;
+  }
+}
+
+const removeWish = async (productId) => {
+  try {
+    return await database.query(
+      `DELETE
+      FROM wishlist
+      WHERE product_id = ? 
+  `, [productId]
+    )
+  } catch (err) {
+    const error = new Error('INVALID_DATA_INPUT(removeWish)');
+    error.statusCode = 500;
+    throw error;
+  }
+}
+
+const checkWishlist = async (productId, userId) => {
+  try {
+    return await database.query(
+      `SELECT
+      product_id
+      FROM wishlist
+      WHERE product_id = ? AND user_id = ?
+      `, [productId, userId]
+    )
+  }
+  catch (err) {
+    const error = new Error('INVALID_DATA_INPUT');
+    error.statusCode = 500;
+    throw error;
+  }
+}
+module.exports = {
+  createWish,
+  removeWish,
+  checkWishlist,
+  showWish
+}
