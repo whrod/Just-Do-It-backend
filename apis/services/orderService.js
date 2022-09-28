57240b4a3fcc8e28969ffbc2528e62e02d4746da
whrod <whrodtube@gmail.com>
2022-09-28 15:26:22 +0900

    wip

--- apis/services/orderService.js
+++ apis/services/orderService.js
@@ -1,5 +1,6 @@
-const { orderDao } = require('../models');
+const { orderDao, cartDao } = require('../models');
 const { checkStock } = require('../utils/checkStock');
+const { getCartsByUserId } = require('../models/cartDao');
 
 const orderInDetail = async (productOptionId, quantity) => {
   await checkStock(productOptionId, quantity);
@@ -9,6 +10,8 @@ const orderInDetail = async (productOptionId, quantity) => {
 
 //재고 관리 필요
 const orderInCart = async (userId) => {
+  const getCartsforOrder = await cartDao.getCartsByUserId(userId);
+  console.log(getCartsforOrder);
   return await orderDao.orderInCart(userId);
 };
 
