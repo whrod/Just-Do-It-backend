9fbcef4e94b9fdcb3700138dbc624aff9886c5b3
whrod <whrodtube@gmail.com>
2022-09-28 16:19:40 +0900

    orderInCart 에러핸들링

--- apis/services/orderService.js
+++ apis/services/orderService.js
@@ -1,6 +1,5 @@
 const { orderDao, cartDao } = require('../models');
 const { checkStock } = require('../utils/checkStock');
-const { getCartsByUserId } = require('../models/cartDao');
 
 const orderInDetail = async (productOptionId, quantity) => {
   await checkStock(productOptionId, quantity);
@@ -8,11 +7,28 @@ const orderInDetail = async (productOptionId, quantity) => {
   await orderDao.orderInDetail(productOptionId, quantity);
 };
 
-//재고 관리 필요
 const orderInCart = async (userId) => {
   const getCartsforOrder = await cartDao.getCartsByUserId(userId);
-  console.log(getCartsforOrder);
-  return await orderDao.orderInCart(userId);
+  for (let i = 0; i < getCartsforOrder.length; i++) {
+    console.log(getCartsforOrder[i].stock);
+    console.log(getCartsforOrder[i].quantity);
+
+    if (getCartsforOrder[i].stock >= getCartsforOrder[i].quantity) {
+      const result = await orderDao.orderInCart(userId);
+      await cartDao.deleteAllCarts(userId);
+      return result;
+    }
+    if (getCartsforOrder[i].stock !== getCartsforOrder[i].quantity) {
+      const error = new Error(`INVALID QUANTITY OF REQUEST`);
+      error.statusCode = 400;
+      throw error;
+    }
+    if (getCartsforOrder[i].stock === 0) {
+      const error = new Error('ITEMS_IN_YOUR_CART_ARE_OUT_OF_STOCK');
+      error.statusCode = 400;
+      throw error;
+    }
+  }
 };
 
 module.exports = {
