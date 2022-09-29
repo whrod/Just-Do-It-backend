2da42c2b2edbe10d7c4286832d58625b89d47d51
whrod <whrodtube@gmail.com>
2022-09-29 11:27:03 +0900

    getStock 수정

--- apis/models/cartDao.js
+++ apis/models/cartDao.js
@@ -103,7 +103,6 @@ const getStock = async (productOptionId) => {
   const [productOption] = await database.query(
     `
     SELECT
-        product_id
         stock
     FROM product_options
     WHERE id = ?    
