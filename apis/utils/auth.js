8b8671b002ae148b91dd3f6d72adf4c7bcd0e5d7
yunah <yunah.jeongg@gmail.com>
2022-09-29 21:46:41 +0900

    modify

--- apis/utils/auth.js
+++ apis/utils/auth.js
@@ -21,23 +21,6 @@ const loginRequired = catchAsync(async (req, res, next) => {
   next();
 });
 
-const checkUserId = catchAsync(async (req, res, next) => {
-  const accessToken = req.headers.authorization;
-  if (!accessToken) {
-    req.user = null;
-    return next();
-  }
-
-  const verifyToken = jwt.verify(accessToken, process.env.JWT_SECRET);
-  const user = await userService.getUserById(verifyToken.id);
-
-  if (!user) {
-    req.userId = null;
-  }
-
-  req.user = user;
-  next();
-});
 
 const checkUserId = catchAsync(async (req, res, next) => {
   const accessToken = req.headers.authorization;
