86de66b33ff6bd8f781cc8a313f14c72c19b4eaf
whrod <whrodtube@gmail.com>
2023-01-04 15:28:40 +0900

    Feat : swagger structure

---
+++ apis/utils/swagger.js
@@ -0,0 +1,21 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'JustDoIt API',
+      version: '1.0.0',
+      description: 'just-do-it-project API',
+    },
+    host: 'localhost:8000',
+    basePath: '/',
+  },
+  apis: ['../routes/index.js', './swagger/*'],
+};
+const specs = swaggerJsdoc(options);
+
+module.exports = {
+  swaggerUi,
+  specs,
+};
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const routes = require('./apis/routes');
+const { swaggerUi, specs } = require('./apis/utils/swagger');
 const { globalErrorHandler } = require('./apis/utils/error');
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.all('*', (req, res, next) => {
   const error = new Error(`Can't fine ${req.originalUrl} on this server!`);
@@ -25,5 +27,5 @@ app.use(globalErrorHandler);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Listening to request on 127.0.0.1:${PORT}`);
+  console.log(`Listening to request on Port:${PORT}`);
 });
