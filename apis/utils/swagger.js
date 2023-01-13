8d449825cc1978d7b94ae9fa572c97a1e68c9558
whrod <whrodtube@gmail.com>
2023-01-14 03:49:31 +0900

    Fix : swagger server add

--- apis/utils/swagger.js
+++ apis/utils/swagger.js
@@ -24,7 +24,16 @@ const options = {
       bearerAuth: [],
     },
     persistAuthorization: true,
-    host: 'localhost:8000',
+    servers: [
+      {
+        url: 'http://localhost:8000',
+        description: 'Test Server In Local',
+      },
+      {
+        url: 'http://3.36.93.92:8000',
+        description: 'API Documentation in EC2',
+      },
+    ],
   },
   apis: ['apis/routes/*.js'],
 };
